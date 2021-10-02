77bc21200df5b5806308dcb6a926d796ca5d1c4f
gustavomena <gustavo.mena@gmail.com>
2021-10-02 02:52:11 -0300

    adding footer elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ document.getElementById("txtCard1").innerHTML = whoWeAre_Card1;
 
 const whoWeAre_Card2="Nulla vehicula urna a velit porttitor vulputate. Proin pulvinar nunc in volutpat tempus. Nulla accumsan augue sapien, nec dictum mi interdum malesuada. Curabitur nec eros a lectus vulputate sollicitudin. Maecenas vel consectetur diam. Fusce magna sapien, sagittis eu tortor sed, rhoncus efficitur tortor. Sed ultrices eget ante ac feugiat. Etiam venenatis, erat non fermentum hendrerit, lorem purus aliquam lorem, non dictum ligula ante vel justo. In eu sapien ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis at magna sit amet dui aliquet bibendum. Sed non pharetra justo, nec faucibus tortor. Vivamus interdum leo at massa euismod sodales.";
 
-document.getElementById("txtCard2").innerHTML = whoWeAre_Card1;
+document.getElementById("txtCard2").innerHTML = whoWeAre_Card2;
 
 const whoWeAre_Card3="Vestibulum lobortis sodales dictum. Suspendisse placerat, enim nec aliquam ultrices, metus lorem ullamcorper massa, id vehicula urna ante ac mauris. Sed eleifend vitae quam quis fermentum. Integer aliquet odio nibh, sit amet dictum nisl faucibus sed. Duis dignissim nec mi sit amet fringilla. Curabitur mollis feugiat mattis. In placerat felis vel libero maximus, quis tempor est accumsan. Nulla mattis ligula congue lacus dignissim, sit amet interdum est fermentum. Vivamus quis nisl porttitor, vulputate metus ut, consequat purus. Nulla non vehicula nunc, sed vestibulum lacus.";
 
-document.getElementById("txtCard3").innerHTML = whoWeAre_Card1;
+document.getElementById("txtCard3").innerHTML = whoWeAre_Card3;
