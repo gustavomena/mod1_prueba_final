2d4cd10138a94248b46826b22b26b00136a6be28
gustavomena <gustavo.mena@gmail.com>
2021-10-01 23:44:52 -0300

    html structure completed

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const whoWeAre_Card1="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin varius lorem sed tortor ullamcorper placerat. Aenean facilisis feugiat urna a consectetur. Donec a dui posuere, porta elit eu, rhoncus metus. Phasellus nec dolor suscipit, aliquet orci ut, auctor purus. Donec ornare sollicitudin maximus. Quisque non dapibus turpis, et sodales massa. Nam ac erat aliquam";
+
+document.getElementById("txtCard1").innerHTML = whoWeAre_Card1;
+
+const whoWeAre_Card2="Nulla vehicula urna a velit porttitor vulputate. Proin pulvinar nunc in volutpat tempus. Nulla accumsan augue sapien, nec dictum mi interdum malesuada. Curabitur nec eros a lectus vulputate sollicitudin. Maecenas vel consectetur diam. Fusce magna sapien, sagittis eu tortor sed, rhoncus efficitur tortor. Sed ultrices eget ante ac feugiat. Etiam venenatis, erat non fermentum hendrerit, lorem purus aliquam lorem, non dictum ligula ante vel justo. In eu sapien ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis at magna sit amet dui aliquet bibendum. Sed non pharetra justo, nec faucibus tortor. Vivamus interdum leo at massa euismod sodales.";
+
+document.getElementById("txtCard2").innerHTML = whoWeAre_Card1;
+
+const whoWeAre_Card3="Vestibulum lobortis sodales dictum. Suspendisse placerat, enim nec aliquam ultrices, metus lorem ullamcorper massa, id vehicula urna ante ac mauris. Sed eleifend vitae quam quis fermentum. Integer aliquet odio nibh, sit amet dictum nisl faucibus sed. Duis dignissim nec mi sit amet fringilla. Curabitur mollis feugiat mattis. In placerat felis vel libero maximus, quis tempor est accumsan. Nulla mattis ligula congue lacus dignissim, sit amet interdum est fermentum. Vivamus quis nisl porttitor, vulputate metus ut, consequat purus. Nulla non vehicula nunc, sed vestibulum lacus.";
+
+document.getElementById("txtCard3").innerHTML = whoWeAre_Card1;
